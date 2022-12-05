45e043d167a337cdfa37c2c7acf47d49580d4b6a
Balint Orosz <orosz.balint.97@gmail.com>
2022-12-05 17:16:22 +0100

    add day-5

---
+++ day-5/b.ts
@@ -0,0 +1,46 @@
+const input = await Deno.readTextFile("./input.txt");
+
+const [stacks, instructions]: string[] = input.split("\n\n");
+
+const instructionLines = instructions.split("\n").filter((instructionLine) =>
+  !!instructionLine
+);
+const stackLines = stacks.split("\n");
+
+const columns = stackLines.pop()?.split(" ").filter(
+  (stackLine) => !!stackLine,
+) ?? [];
+
+const reversedStackLine = stackLines.reverse();
+
+const mappedStack = columns.map((_column, index) => {
+  return reversedStackLine.reduce((acc, stackLine) => {
+    return [...acc, stackLine.substring(index * 4, index * 4 + 3)];
+  }, [] as string[]).filter((stackLine) => !/\ \ \ /.test(stackLine));
+});
+
+const int = instructionLines.map((instructionLines) => {
+  return instructionLines.replace("move ", "").replace(" from ", ",").replace(
+    " to ",
+    ",",
+  ).split(",").reduce((acc, n) => {
+    return [...acc, parseInt(n)];
+  }, [] as number[]);
+});
+
+int.forEach(([move, from, to]) => {
+  const arr: string[] = []
+  for (let index = 0; index < move; index++) {
+    arr.push(mappedStack[from - 1].pop() ?? '');
+  }
+  arr.reverse()
+
+  arr.forEach(a => {
+    mappedStack[to - 1].push(a ?? "");
+  })
+});
+
+mappedStack.forEach((stack) => {
+  console.log(stack[stack.length - 1]);
+});
+
